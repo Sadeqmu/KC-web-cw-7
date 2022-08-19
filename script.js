4c7231255a104c2b8910b8ab1ce9d2a3dc63311c
Sadeqmu <sadeq3370@gmail.com>
2022-08-19 17:34:52 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+let Status1 = document.getElementById('result') ;
 
+function BMI(weight,height){
+    let result = weight / (height*height);
+    return result;
+}
+console.log(BMI());
+function Status (BMI) {
+    if(BMI < 18.5){
+        Status1.style.color = 'orange'
+        return `لديك نقص في الوزن`;
+}
+    else if(BMI>=18.5 && BMI<25){
+        Status1.style.color = 'green'
+        return ` وزنك صحي` ;
+    }
+    else if(BMI>=25){
+        Status1.style.color = 'red'
+        return `لديك زيادة في الوزن`;
+    } else {
+        return `error`;
+    }
+}
+function calculate (){
+    let weight = document.getElementById('weight').value;
+    let height = document.getElementById('height').value;
+    let bmi = BMI(weight,height);
+    let desc = Status(bmi)
+    let div = document.getElementById('result')
+    div.innerText = bmi + " ==> "   + desc;
+}
